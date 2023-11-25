c7ffcd5d9785b16cbb593b07875a8ad2e1f03f73
Frigesty <frigesty@gmail.com>
2023-11-25 14:52:06 +0300

    add tag

--- src/test/java/ru/frigesty/tests/HomeTest.java
+++ src/test/java/ru/frigesty/tests/HomeTest.java
@@ -1,5 +1,7 @@
 package ru.frigesty.tests;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import ru.frigesty.pages.RegistrationPage;
 
@@ -9,50 +11,51 @@ public class HomeTest extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
     GenerationData gD = new GenerationData();
-
+    @Tag("simple")
     @Test
+    @DisplayName("Проверка тестовой формы")
     void practiceFormTest() {
 
-        step("Открываем страницу", () -> {
-            registrationPage.openPage();
-        });
-        step("Закрываем банер и футер", () -> {
-            registrationPage.removeFooter();
-        });
+        step("Открываем страницу", () ->
+            registrationPage.openPage()
+        );
+        step("Закрываем банер и футер", () ->
+            registrationPage.removeFooter()
+        );
         step("Вводим имя и фамилию", () -> {
             registrationPage.setFirstName(gD.firstName);
             registrationPage.setLastName(gD.lastName);
         });
-        step("Вводим email", () -> {
-            registrationPage.userEmailName(gD.userEmail);
-        });
-        step("Выбираем гендер", () -> {
-            registrationPage.chooseGender(gD.userGender);
-        });
-        step("Вводим номер телефона", () -> {
-            registrationPage.userMobileNumber(gD.userNumber);
-        });
-        step("Вводим дату рождения", () -> {
-            registrationPage.chooseBirthDate(gD.dayOfBirth, gD.monthOfBirth, gD.yearOfBirth);
-        });
-        step("Выбираем предметы", () -> {
-            registrationPage.writeAndChooseSubject(gD.subject);
-        });
-        step("Выбираем хобби", () -> {
-            registrationPage.chooseHobbies(gD.hobbies);
-        });
-        step("Загружаем картинку", () -> {
-            registrationPage.uploadPicture(gD.pictures);
-        });
-        step("Вводим адрес", () -> {
-            registrationPage.setAddressName(gD.currentAddress);
-        });
-        step("Выбираем штат и город", () -> {
-            registrationPage.chooseStateAndCity(gD.randomState, gD.randomCity);
-        });
-        step("Кликаем на кнопку Submit", () -> {
-            registrationPage.clickSubmit();
-        });
+        step("Вводим email", () ->
+            registrationPage.userEmailName(gD.userEmail)
+        );
+        step("Выбираем гендер", () ->
+            registrationPage.chooseGender(gD.userGender)
+        );
+        step("Вводим номер телефона", () ->
+            registrationPage.userMobileNumber(gD.userNumber)
+        );
+        step("Вводим дату рождения", () ->
+            registrationPage.chooseBirthDate(gD.dayOfBirth, gD.monthOfBirth, gD.yearOfBirth)
+        );
+        step("Выбираем предметы", () ->
+            registrationPage.writeAndChooseSubject(gD.subject)
+        );
+        step("Выбираем хобби", () ->
+            registrationPage.chooseHobbies(gD.hobbies)
+        );
+        step("Загружаем картинку", () ->
+            registrationPage.uploadPicture(gD.pictures)
+        );
+        step("Вводим адрес", () ->
+            registrationPage.setAddressName(gD.currentAddress)
+        );
+        step("Выбираем штат и город", () ->
+            registrationPage.chooseStateAndCity(gD.randomState, gD.randomCity)
+        );
+        step("Кликаем на кнопку Submit", () ->
+            registrationPage.clickSubmit()
+        );
         step("Проверяем результат", () -> {
             registrationPage.verifyRegistrationResultsModalAppears()
                     .verifyResult("Student Name", gD.firstName + " " + gD.lastName)
